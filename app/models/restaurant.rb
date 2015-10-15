6b93a6772875d8192cc0fdb7ea7d5e79affb70f2
Michael Roger <bodgeuk12@gmail.com>
2015-10-15 10:54:19 +0100

    Each review has a user ID

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,12 +1,10 @@
 class Restaurant < ActiveRecord::Base
 
-  has_many :reviews, dependent: :destroy
   belongs_to :user
   validates :name, length: { minimum: 3 }, uniqueness: true
 
-  def build_review(attributes = {}, user)
-    review = reviews.build(attributes)
-    review.user = user
-    review
-  end
+  has_many :reviews,
+      -> { extending WithUserAssociationExtension },
+      dependent: :destroy
+
 end
