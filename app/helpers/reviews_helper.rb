38137eccb7399a06768ed42f2dc00fa9fb35b7b7
chweeks <chrisharryweeks@gmail.com>
2015-10-15 17:42:56 +0100

    added round method to average_rating

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -4,12 +4,12 @@ module ReviewsHelper
     total_rating = 0
     return "N/A" if reviews.none?
     reviews.each do |review|
-      total_rating += review.rating
+    total_rating += review.rating
     end
     total_rating / reviews.length
   end
 
   def star_rating
-    ('★' * (self.average_rating)) + '☆' * (5-self.average_rating)
+    ('★' * (self.average_rating.round)) + '☆' * (5-self.average_rating)
   end
 end
