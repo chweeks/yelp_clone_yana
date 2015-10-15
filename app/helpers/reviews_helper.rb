d0be248b6831e81643cc6d1d2e292c0b66cb8b16
chweeks <chrisharryweeks@gmail.com>
2015-10-15 16:44:37 +0100

    added rating methods to ReviewsHelper module

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,15 @@
 module ReviewsHelper
+
+  def average_rating
+    total_rating = 0
+    return "N/A" if reviews.none?
+    reviews.each do |review|
+      total_rating += review.rating
+    end
+    total_rating / reviews.length
+  end
+
+  def star_rating
+    ('★' * (self.average_rating)) + '☆' * (5-self.average_rating)
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,19 +1,12 @@
 class Restaurant < ActiveRecord::Base
 
+  include ReviewsHelper
+
   belongs_to :user
   validates :name, length: { minimum: 3 }, uniqueness: true
 
   has_many :reviews,
       -> { extending WithUserAssociationExtension },
       dependent: :destroy
-
-  def average_rating
-    total_rating = 0
-    return "N/A" if reviews.none?
-    reviews.each do |review|
-      total_rating += review.rating
-    end
-    total_rating / reviews.length
-  end
 
 end
