112ad43f2a4410ea8826ec0d0bf96ea53f54c60e
Michael Roger <bodgeuk12@gmail.com>
2015-10-15 10:09:03 +0100

    Thursday

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -9,7 +9,7 @@ feature 'reviewing' do
     sign_up(user)
   end
 
-  scenario 'allows users to leave a reviewusing a form' do
+  scenario 'allows users to leave a review using a form' do
     visit '/restaurants'
     click_link "Review #{restaurant.name}"
     fill_in "Thoughts", with: "so so"
@@ -29,6 +29,19 @@ feature 'reviewing' do
     click_link "Delete #{restaurant.name}"
     expect(page).not_to have_content "so so"
     expect(current_path).to eq '/restaurants'
+  end
+
+  scenario 'users cannot review the same restaurant twice' do
+    visit '/restaurants'
+    click_link "Review #{restaurant.name}"
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+    click_link "Review #{restaurant.name}"
+    fill_in "Thoughts", with: "Still so so"
+    select '2', from: 'Rating'
+    click_button 'Leave Review'
+    expect(page).to have_content 'You have alreay reviewed this restaurant'
   end
 
 end
